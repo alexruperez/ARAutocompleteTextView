8c20b1e355b9505eb968e035be441cbcadbe756a
Lucas Farah <lucas.farah@me.com>
2015-09-10 19:06:57 -0700

    Added Swift Example

---
+++ ARTextViewAutocompletionExampleSwift/ARTextViewAutocompletionExampleSwift-bridging-header.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ARTextViewAutocompletionExampleSwift
+//
+//  Created by Lucas Farah on 9/10/15.
+//  Copyright (c) 2015 Lucas Farah. All rights reserved.
+//
+
+#import "ARAutocompleteTextView.h"
+#import "AREmailAutocompleteTextView.h"
+#import "ARTwitterAutocompleteTextView.h"
+#import "ARAutocompleteManager.h"
+
